feb5147fcc0351a3216e242326f3bb3c84fb0f3e
Leon Elliott Fuller <l.elliottfuller@gmail.com>
2023-01-02 15:17:45 +0100

    Implementacion de Solver.cpp y Solver.h

--- ej04_juego_letras/estudiante/include/Solver.h
+++ ej04_juego_letras/estudiante/include/Solver.h
@@ -8,6 +8,8 @@
 #include <vector>
 #include "dictionary.h"
 #include "letters_set.h"
+#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
@@ -32,12 +34,33 @@ public:
     pair <vector<std::string>,int> getSolutions(const vector<char> & available_letters, bool score_game);
 
     /**
- * @brief Auxiliar function to get all the words formed by a tree
- * @param curr_node Current node of the tree
- * @param word String that stores the current word formed by the tree
- * @param words Vector that will store all the words formed by the tree
- */
-    void getWords(const Dictionary::iterator curr_node, string word, vector<string>& words);
+     * @brief Auxiliar function to get all the words formed by a tree
+     * @param curr_node Current node of the tree
+     * @param word String that stores the current word formed by the tree
+     * @param words Vector that will store all the words formed by the tree
+     */
+    void getWords(vector<string>& words);
+
+    /**
+     * @brief Get all the solutions that can be formed using the available letters
+     *
+     * @param available_letters Vector of available letters
+     * @param words Vector of words to be checked
+     * @return Vector of strings containing the solutions that can be formed
+     */
+    vector<string> getPossibleWords(const vector<char> &available_letters, const vector<string> &words);
+
+    /**
+     * @brief Ordena las palabras por su puntuacion
+     * @param solutions
+     */
+    void compareByPoints(vector<string> &solutions);
+
+    /**
+     * @brief Ordena las longitudes de palabras de menor a mayor
+     * @param solutions
+     */
+    void compareByLength(vector<string> &solutions);
 
 };
 
--- ej04_juego_letras/estudiante/src/Solver.cpp
+++ ej04_juego_letras/estudiante/src/Solver.cpp
@@ -14,36 +14,87 @@ pair<vector<std::string>, int> Solver::getSolutions(const vector<char> &availabl
     vector<string> palabras;
     soluciones.second = 0; // Initialize score to 0
 
-    // Check all possible words in the dictionary
-    getWords(dictionary.begin(),"",palabras);
+    //Devuelve todas las palabras del diccionario
+    getWords(palabras);
+    //Devuelve todas las palabras formadas
+    soluciones.first = this->getPossibleWords(available_letters,palabras);
 
-    // Sort the resulting words in descending order of score
-    sort(soluciones.first.begin(), soluciones.first.end(), [score_game](const std::string& a, const std::string& b) {
-        if (score_game) {
-            return getPoints(a) > getPoints(b);
-        } else {
-            return a.length() > b.length();
+    //Ordenamos las palabras en funcion de longitud o puntuacion
+    if (score_game)
+        this->compareByPoints(soluciones.first);
+    else
+        this->compareByPoints(soluciones.first);
+
+    //Creamos ya el pair que contenga las mejores soluciones
+    int maxima_puntuacion = this->letters.getScore(soluciones.first.back());
+    pair<vector<string>,int> solucion_final;
+    solucion_final.second = maxima_puntuacion;
+    solucion_final.first.push_back(soluciones.first.back());
+    soluciones.first.pop_back();
+    while(this->letters.getScore(soluciones.first.back()) == maxima_puntuacion){
+        solucion_final.first.push_back(soluciones.first.back());
+        soluciones.first.pop_back();
+    }
+
+    return solucion_final;
+}
+
+void Solver::getWords(vector<string>& words) {
+    for (Dictionary::iterator it = this->dictionary.begin(); it != this->dictionary.end(); ++it){
+        words.push_back(it.operator*());
+    }
+}
+
+vector<string> Solver::getPossibleWords(const vector<char> &available_letters, const vector<string> &words) {
+    std::vector<std::string> solutions;
+
+    // For each word in the vector of words
+    for (const string &word : words) {
+        // Check if the word can be formed with the available letters
+        bool can_form = true;
+        unordered_set<char> used_letters;
+        for (char c : word) {
+            if (used_letters.count(c) || (find(available_letters.begin(), available_letters.end(), c) == available_letters.end())){
+                can_form = false;
+                break;
+            }
+            used_letters.insert(c);
+        }
+
+        // If the word can be formed, add it to the vector of solutions
+        if (can_form) {
+            solutions.push_back(word);
         }
     }
+
+    return solutions;
 }
 
-void Solver::getWords(const Dictionary::iterator& curr_node, string word, vector<string>& words) {
-    // If current node is not a leaf, add its character to the current word
-    if(curr_node.right() || curr_node.left()) {
-        word += *curr_node;
-    }
-
-    // If current node is a valid word, add it to the vector
-    if((*curr_node).valid_word) {
-        words.push_back(word);
-    }
-
-    // Recursively call the function on the left child and right sibling
-    if(curr_node.left()) {
-        getWords(curr_node.left(), word, words);
-    }
-    if(curr_node.right()) {
-        getWords(curr_node.right(), word, words);
+void Solver::compareByPoints(vector<string> &solutions) {
+    for (int i = 0; i < solutions.size() - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < solutions.size(); j++) {
+            if (this->letters.getScore(solutions[j]) < this->letters.getScore(solutions[min_idx])) {
+                min_idx = j;
+            }
+        }
+        swap(solutions[i], solutions[min_idx]);
     }
 }
+
+void Solver::compareByLength(vector<string> &solutions) {
+    for (size_t i = 0; i < solutions.size() - 1; i++) {
+        // Find the minimum element in the unsorted part of the vector
+        size_t min_idx = i;
+        for (size_t j = i + 1; j < solutions.size(); j++) {
+            if (solutions[j].length() < solutions[min_idx].length()) {
+                min_idx = j;
+            }
+        }
+        // Swap the minimum element with the element at the current position
+        swap(solutions[i], solutions[min_idx]);
+    }
+}
+
+
 
