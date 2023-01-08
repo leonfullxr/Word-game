e873f0136871c8de3eacd61df8711d5a4e6cf017
Leon Elliott Fuller <l.elliottfuller@gmail.com>
2023-01-08 18:44:44 +0100

    Actualizacion de la documentacion doxygen de la clase solver.h

--- ej04_juego_letras/estudiante/include/solver.h
+++ ej04_juego_letras/estudiante/include/solver.h
@@ -15,15 +15,27 @@
 using namespace std;
 
 /**
- * @class todo
+ * @class Solver que extrae las soluciones con el iterador de la clase Dictionary
  */
 class Solver {
 private:
     Dictionary dictionary;
     LettersSet letters;
 
+    /**
+     * @brief Funcion auxiliar que calcula la puntuacion de una palabra dependiendo del modo de juego
+     * @param word El string a calcular la puntuacion
+     * @param score_game True si la puntuacion es a puntuacion calculada en el metodo getScore de LetterSet o false en caso de longitud
+     * @return un entero con el valor de la puntuacion de la palabra
+     */
     int getScore(const string & word, bool score_game);
 
+    /**
+     * @brief Funcion auxiliar que comprueba si la palabra @word se puede construir con las letras disponibles
+     * @param word La palabra a comprobar
+     * @param available_letters Las letras disponibles para construir la palabra @word
+     * @return True en caso de que se pueda construir y false en caso contrario
+     */
     bool isValid(const string & word, multiset<char> available_letters);
 
 public:
@@ -45,13 +57,19 @@ public:
 
 
 /**
- * @class todo
+ * @class EfficientSolver que extrae las soluciones con el iterador eficiente de la clase Dictionary
  */
 class EfficientSolver {
 private:
     Dictionary dictionary;
     LettersSet letters;
 
+    /**
+     * @brief Funcion auxiliar que calcula la puntuacion de una palabra dependiendo del modo de juego
+     * @param word El string a calcular la puntuacion
+     * @param score_game True si la puntuacion es a puntuacion calculada en el metodo getScore de LetterSet o false en caso de longitud
+     * @return un entero con el valor de la puntuacion de la palabra
+     */
     int getScore(const string & word, bool score_game);
 public:
     /**
